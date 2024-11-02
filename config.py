99f67b121849fd0c3af4a6b0f596eda3892449da
Dijest54 <147441670+Dijest54@users.noreply.github.com>
2024-11-02 03:02:43 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "8071417488:AAFzssXtpY4ZJuQpzNa3IvXzNHpcssVTF8I"  # токен бота
+CHANNEL_URL = "https://t.me/+JDRrol3AZY9mNTUy"  # Ссылка на канал
+CHANNEL_ID = "-1002374553082"  # id канала. Начинаеться с минуса
+ADMIN_ID = "7377157357" #tg id твой
+
+REF_URL = "https://cutt.ly/neDdsSJm"  # Ссылка на регистрацию
